794d8c6c9eaee154810ed715a56ed77dc8acc097
Andrew Koerber <kerbybit@gmail.com>
2018-08-16 08:58:11 -0500

    Added a way to copy out the errors for pasting in discord

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+var ToolKit = Java.type("java.awt.Toolkit");
+var StringSelection = Java.type("java.awt.datatransfer.StringSelection");
+
 register("command", function(type) {
 	if (type) {
 		new Thread(function() {
+			Stresser.failed = [];
+			
 			ChatLib.chat("&9&m" + ChatLib.getChatBreak("-"));
 			
 			if (Stresser.tests.containsKey(type.toLowerCase())) {
@@ -10,7 +15,15 @@ register("command", function(type) {
 			}
 			
 			ChatLib.chat(Stresser.completed + "/" + Stresser.tested + " passed.");
-			ChatLib.chat("&9&m" + ChatLib.getChatBreak("-"));
+			if (Stresser.failed.length > 0) {
+				new Message(
+					new TextComponent("&c[copy errors]")
+						.setClick("run_command", "/test.copyErrors")
+						.setHoverValue("&7Click to copy errors to clipboard")
+				).setChatLineId(5474).chat();
+			}
+			
+			new Message("&9&m" + ChatLib.getChatBreak("-")).setChatLineId(5475).chat();
 			
 			Stresser.tested = 0;
 			Stresser.completed = 0;
@@ -18,6 +31,8 @@ register("command", function(type) {
 		}).start();
 	} else {
 		new Thread(function() {
+			Stresser.failed = [];
+			
 			ChatLib.chat("&9&m" + ChatLib.getChatBreak("-"));
 			Stresser.tests.forEach(function(key, value) {
 				Stresser.message.addTextComponent(
@@ -27,16 +42,42 @@ register("command", function(type) {
 			});
 			
 			ChatLib.chat(Stresser.completed + "/" + Stresser.tested + " passed.");
-			ChatLib.chat("&9&m" + ChatLib.getChatBreak("-"));
+			if (Stresser.failed.length > 0) {
+				new Message(
+					new TextComponent("&c[copy errors]")
+						.setClick("run_command", "/test.copyErrors")
+						.setHoverValue("&7Click to copy errors to clipboard")
+				).setChatLineId(5474).chat();
+			}
+			
+			new Message("&9&m" + ChatLib.getChatBreak("-")).setChatLineId(5475).chat();
 	
 			Stresser.tested = 0;
 			Stresser.completed = 0;
 			Stresser.message = new Message().setChatLineId(5473);
 		}).start();
 	}
-	
-	
 }).setName("test");
+
+register("command", function() {
+	var copied = "```asciidoc\n"
+	
+	for (var i = 0; i < Stresser.failed.length; i++) {
+		copied += Stresser.failed[i] + "\n";
+	}
+	
+	copied += "```";
+	
+	Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(copied), null);
+	
+	new Message(
+		new TextComponent("&aCopied!")
+			.setClick("run_command", "/test.copyErrors")
+			.setHoverValue("&7Click to copy again")
+	).setChatLineId(5474).chat();
+	
+	new Message("&9&m" + ChatLib.getChatBreak("-")).setChatLineId(5475).chat();
+}).setName("test.copyErrors");
 
 function out(arg) {
 	Stresser.tested++;
@@ -48,6 +89,7 @@ function out(arg) {
 		Stresser.completed++;
 	} catch(err) {
 		ChatLib.chat(arg + ": \n &c" + err);
+		Stresser.failed.push(arg + " :: " + err);
 		Stresser.message.addTextComponent(
 			new TextComponent("&cX ").setHoverValue(arg + "\n&c" + err)
 		).chat();
@@ -58,5 +100,6 @@ var Stresser = {
 	tests: new HashMap(),
 	message: new Message().setChatLineId(5473),
 	tested: 0,
-	completed: 0
+	completed: 0,
+	failed: []
 };
